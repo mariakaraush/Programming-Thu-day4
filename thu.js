d9143264fb5badffee73b0f119d87fa620a834c7
mariakaraush <167077657+mariakaraush@users.noreply.github.com>
2024-04-18 09:30:54 +0000

    .

---
+++ thu.js
@@ -0,0 +1,26 @@
+// function mutliplyArray(arr) {
+//     let result = 1;
+//     for (const num of arr) {
+//         result *= num
+//     }
+//     return result
+// }
+
+// const num = [2, 4, 6]
+// console.log(mutliplyArray(num))
+
+// const person = ["John", "Doe", 46];
+// console.log(person[2])
+
+
+let examResults = {
+    steve: [99, 98, 95],
+    violet: [56, 53, 88],
+    aqil: [100, 100, 100]
+}
+
+constant.log(examResults.steve)
+// constant.log(examResults.steve[0])
+// constant.log(examResults.steve.length)
+// constant.log(examResults.steve.includes(98))
+// constant.log(examResults.steve.includes("hello"))
